cf49c7014dbda3f691d05e94c90b15631749b589
Navarroseb <sebastnavarro@gmail.com>
2022-02-01 18:53:48 +0000

    pendiente

---
+++ test.js
@@ -0,0 +1,15 @@
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
+test("One dollar should be 106.15 Yen", function(){
+    // importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromDollarToYen(3)).toBe(318.45); 
+})
